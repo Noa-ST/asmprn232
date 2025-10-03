a30101854801ad244aee76515700c916a88383a2
Noa-ST <175372422+Noa-ST@users.noreply.github.com>
2025-10-03 17:43:57 +0700

    1

--- ecommerce-ui/src/api.ts
+++ ecommerce-ui/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7022/api", // sửa thành URL backend sau khi deploy
+  baseURL: "https://asmprn232-3.onrender.com/api",
 });
 
 export default api;
