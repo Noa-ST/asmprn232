dfb9b983bda8c371a1e2c129cc49b8c770131fe2
Noa-ST <175372422+Noa-ST@users.noreply.github.com>
2025-10-03 22:57:27 +0700

    Update api.ts

--- ecommerce-ui/src/api.ts
+++ ecommerce-ui/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://asmprn232-3.onrender.com/api",
+  baseURL: "https://asmprn232-pro.onrender.com/api",
 });
 
 export default api;
